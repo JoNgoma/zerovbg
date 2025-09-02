2373434895d1dac93921bfb619c45ded023b8d21
Josué Ngoma <josuengoma26@gmail.com>
2025-09-02 23:14:35 +0100

    Implement code changes to enhance functionality and improve performance

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,26 @@
 <script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
 import Customizer from '@/static/Customizer.vue';
 import Footer from '@/static/Footer.vue';
 import Header from '@/static/Header.vue';
 import TopButton from '@/static/TobButton.vue';
 
+// Récupérer la route actuelle
+const route = useRoute()
 
-// const userData = ref({
-//   firstName: '',
-//   name: '',
-//   email: ''
-// })
-
+// Déterminer si on doit afficher le footer
+const showFooter = computed(() => route.name !== 'i-ia')
 </script>
 
 <template>
-<div class="be-wrapper be-fixed-sidebar">
-      <Customizer />
-      <Header />
-      <RouterView />
-      <Footer />
-      <TopButton />
-    </div>
+  <div class="be-wrapper be-fixed-sidebar">
+    <Customizer />
+    <Header />
+    <RouterView />
+    <!-- Afficher le footer seulement si showFooter est vrai -->
+    <Footer v-if="showFooter" />
+    <TopButton />
+  </div>
 </template>
-
